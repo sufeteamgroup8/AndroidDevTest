b115d5ec9c97339b99b708bebffd67e44690f204
nielei <nielei1999@163.com>
2020-12-16 10:44:22 +0800

    补全DialogUtil.java,AbsFragmentActivity.java

---
+++ app/src/main/java/com/systemvx/androiddevtest/DialogUtil.java
@@ -0,0 +1,36 @@
+package com.systemvx.androiddevtest;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+public class DialogUtil {
+    public static void showDialog(Context ctx, String msg, boolean goHome)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(ctx)
+                .setMessage(msg).setCancelable(false);
+        if(goHome)
+        {
+            builder.setPositiveButton("确定",((dialog, which) -> {
+                Intent i=new Intent(ctx,orderClientActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                ctx.startActivity(i);
+            }));
+        }
+        else
+        {
+            builder.setPositiveButton("确定",null);
+        }
+        builder.create().show();
+    }
+    public static void showDialog(Context ctx, View view)
+    {
+        new AlertDialog.Builder(ctx)
+                .setView(view).setCancelable(false)
+                .setPositiveButton("确定",null)
+                .create()
+                .show();
+    }
+}
+
---
+++ app/src/main/java/com/systemvx/androiddevtest/orderClientActivity.java
@@ -0,0 +1,12 @@
+package com.systemvx.androiddevtest;
+
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class orderClientActivity {
+    public orderClientActivity(FragmentActivity activity) {
+    }
+
+
+}
