aa8f2ad3775ba9312bf9f0a010084ca51766015a
nielei <nielei1999@163.com>
2021-01-02 20:43:26 +0800

    orderManagement页面over

---
+++ app/src/main/java/com/systemvx/androiddevtest/ui/orderManagement/adapter/FragmentAdapter.java
@@ -0,0 +1,28 @@
+package com.systemvx.androiddevtest.ui.orderManagement.adapter;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    List<Fragment> fragmentList = new ArrayList<Fragment>();
+    public FragmentAdapter(FragmentManager fm,List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
