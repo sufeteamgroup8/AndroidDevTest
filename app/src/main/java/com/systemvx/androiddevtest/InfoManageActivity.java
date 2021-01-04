e49f371cc7631c1d4ae4aa6da89648b78f697b2b
houguo0409 <807835756@qq.com>
2021-01-05 01:39:27 +0800

    个人信息管理页面、充值提现页面、金币管理页面更改

---
+++ app/src/main/java/com/systemvx/androiddevtest/InfoManageActivity.java
@@ -0,0 +1,14 @@
+package com.systemvx.androiddevtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_manage);
+    }
+}
