5b1afb30aa87aa6c79d1404089b63e6d0bc93b57
aliangyun <aliang_0223@163.com>
2021-01-06 17:45:35 +0800

    添加充值提现页面提醒

--- app/src/main/java/com/systemvx/androiddevtest/ui/payment/PayAndWithdrawActivity.java
+++ app/src/main/java/com/systemvx/androiddevtest/ui/payment/PayAndWithdrawActivity.java
@@ -1,16 +1,32 @@
 package com.systemvx.androiddevtest.ui.payment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.systemvx.androiddevtest.R;
 
-public class PayAndWithdrawActivity extends AppCompatActivity {
+public class PayAndWithdrawActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_and_withdraw);
+        findViewById(R.id.toLogin1).setOnClickListener(this);
+        findViewById(R.id.toLogin2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.toLogin1:
+                ToastUtils.showShort("充值成功");
+                break;
+            case R.id.toLogin2:
+                ToastUtils.showShort("提现成功");
+                break;
+        }
     }
 }
