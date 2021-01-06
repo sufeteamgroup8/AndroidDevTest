44fff06d61372f919503d62ecd73995809381c26
aliangyun <aliang_0223@163.com>
2021-01-06 18:05:59 +0800

    添加充值提现延迟提醒

--- app/src/main/java/com/systemvx/androiddevtest/ui/payment/PayAndWithdrawActivity.java
+++ app/src/main/java/com/systemvx/androiddevtest/ui/payment/PayAndWithdrawActivity.java
@@ -1,7 +1,9 @@
 package com.systemvx.androiddevtest.ui.payment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,8 @@ import com.blankj.utilcode.util.ToastUtils;
 import com.systemvx.androiddevtest.R;
 
 public class PayAndWithdrawActivity extends AppCompatActivity implements View.OnClickListener {
+    private Handler handler = new Handler();
+    private ProgressBar pbBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +20,27 @@ public class PayAndWithdrawActivity extends AppCompatActivity implements View.On
         setContentView(R.layout.activity_pay_and_withdraw);
         findViewById(R.id.toLogin1).setOnClickListener(this);
         findViewById(R.id.toLogin2).setOnClickListener(this);
+        pbBar = findViewById(R.id.pb_bar);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.toLogin1:
-                ToastUtils.showShort("充值成功");
+                pbBar.setVisibility(View.VISIBLE);
+                handler.postDelayed(() -> {
+                    ToastUtils.showShort("充值成功");
+                    pbBar.setVisibility(View.GONE);
+                }, 1500);
+
                 break;
             case R.id.toLogin2:
-                ToastUtils.showShort("提现成功");
+                pbBar.setVisibility(View.VISIBLE);
+                handler.postDelayed(() -> {
+                    pbBar.setVisibility(View.GONE);
+                    ToastUtils.showShort("提现成功");
+                }, 1500);
+
                 break;
         }
     }
