c5d4925a7489c0a45a53997d7ba644e1375e4dcb
hu wen bin <1>
2021-01-06 20:23:00 +0800

    comment 重构

--- app/src/main/java/com/systemvx/androiddevtest/data/coinDataSource.kt
+++ app/src/main/java/com/systemvx/androiddevtest/data/coinDataSource.kt
@@ -1,10 +1,21 @@
 package com.systemvx.androiddevtest.data
 
+import com.systemvx.androiddevtest.data.model.BasicDataSource
 import com.systemvx.androiddevtest.data.model.CoinTrans
 
-class CoinDataSource {
-    fun viewCoin(accountID: Int): Result<ArrayList<CoinTrans>> {
-        TODO("Not yet implemented")
+class CoinDataSource : BasicDataSource() {
+    fun viewCoinTrans(accountID: Int): Result<List<CoinTrans>> {
+        val params = HashMap<String, String>()
+        params["accountID"] = accountID.toString()
+        return getDataList("/coin/view", params, CoinTrans::class.java)
+    }
+
+    fun makePayment(accountID: Int, amount: Double): Result<String> {
+        TODO()
+    }
+
+    fun makeWithdraw(accountID: Int, amount: Double): Result<String> {
+        TODO()
     }
 
 }
